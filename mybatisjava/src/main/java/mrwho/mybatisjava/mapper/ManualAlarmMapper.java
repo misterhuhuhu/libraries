9ac8d1c804cdb3424fa4547acbfc687d57f65876
MR.WHO <975877475@qq.com>
2024-03-19 10:49:23 +0800

    queue

---
+++ mybatisjava/src/main/java/mrwho/mybatisjava/mapper/ManualAlarmMapper.java
@@ -0,0 +1,7 @@
+package mrwho.mybatisjava.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import mrwho.mybatisjava.entity.ManualAlarm;
+
+public interface ManualAlarmMapper extends BaseMapper<ManualAlarm> {
+}
---
+++ mybatisjava/src/main/java/mrwho/mybatisjava/service/FireCabinetStaffProfileService.java
@@ -0,0 +1,11 @@
+package mrwho.mybatisjava.service;
+
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import mrwho.mybatisjava.mapper.FireCabinetStaffProfileMapper;
+import mrwho.mybatisjava.entity.FireCabinetStaffProfile;
+@Service
+public class FireCabinetStaffProfileService extends ServiceImpl<FireCabinetStaffProfileMapper, FireCabinetStaffProfile> {
+
+}
